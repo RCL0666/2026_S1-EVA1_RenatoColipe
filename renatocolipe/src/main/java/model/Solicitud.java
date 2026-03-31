dec1b3fabe301aa96db608d2afc4fce90d480582
Renato Colipe Lienan <re.colipe@duocuc.cl>
2026-03-31 11:10:01 -0300

    validaciones
    Added fields for patient name, specialty, status, registration date, and priority level with validation constraints.

--- renatocolipe/src/main/java/model/Solicitud.java
+++ renatocolipe/src/main/java/model/Solicitud.java
@@ -1,5 +1,32 @@
 package model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+
 public class Solicitud {
+    private int id;
+
+    @NotBlank(message = "El nombre del paciente es obligatorio")
+    private String Paciente;
+
+    @NotBlank(message = "El especialidad es obligatoria")
+    private String Especialidad;
+
+    @NotBlank(message = "El estado es obligatorio")
+    private String Estado; 
+
+    @NotNull(message = "El fecha de registro es obligatoria")
+    private LocalDate FechaRegistro;
+
+    @NotBlank(message = "El Nivel de prioridad es obligatorio")
+    private String NivelPrioridad; 
+
+
+
+
+    
+
 
 }

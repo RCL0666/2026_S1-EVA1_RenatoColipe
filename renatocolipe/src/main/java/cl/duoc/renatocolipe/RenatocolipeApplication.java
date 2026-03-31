9c56123a0c7fd3550f0f4df6200a21ac8eb02206
Renato Colipe Lienan <re.colipe@duocuc.cl>
2026-03-31 10:45:49 -0300

    Add files via upload
    primer comitt

---
+++ renatocolipe/src/main/java/cl/duoc/renatocolipe/RenatocolipeApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.renatocolipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenatocolipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenatocolipeApplication.class, args);
+	}
+
+}

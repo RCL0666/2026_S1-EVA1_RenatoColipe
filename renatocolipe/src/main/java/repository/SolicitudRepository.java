b8b21810e72ffea793945dd990e469e78435da81
Renato Colipe Lienan <re.colipe@duocuc.cl>
2026-03-31 11:23:32 -0300

    se hacen las importaciones
    Added imports for model and Spring framework.

--- renatocolipe/src/main/java/repository/SolicitudRepository.java
+++ renatocolipe/src/main/java/repository/SolicitudRepository.java
@@ -1,5 +1,12 @@
 package repository;
 
+import model.Solicitud;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class SolicitudRepository {
 
 }

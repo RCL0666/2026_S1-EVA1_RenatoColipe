cc32cbe61666137943ed127b7e028b6aa1b8b276
Renato Colipe Lienan <re.colipe@duocuc.cl>
2026-04-02 12:17:15 -0300

    se agregaron las importaciones a  SolicitudService class
    Added necessary imports for SolicitudService.

--- renatocolipe/src/main/java/services/SolicitudService.java
+++ renatocolipe/src/main/java/services/SolicitudService.java
@@ -1,5 +1,11 @@
 package cl.duoc.renatocolipe.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cl.duoc.renatocolipe.model.Solicitud;
+import cl.duoc.renatocolipe.repository.SolicitudRepository;
+
 public class SolicitudService {
 
 }

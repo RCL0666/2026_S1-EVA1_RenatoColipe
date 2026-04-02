dba4b8c775f25c0376ddea1560ccb808a7293bc3
Renato Colipe Lienan <re.colipe@duocuc.cl>
2026-04-02 12:31:29 -0300

    se Implemento operaciones CRUD en SolicitudController

--- renatocolipe/src/main/java/controller/SolicitudController.java
+++ renatocolipe/src/main/java/controller/SolicitudController.java
@@ -14,6 +14,36 @@ import java.util.List;
 @RequestMapping("/api/v1/solicitudes")
 public class SolicitudController {
   @Autowired
-  
+   private SolicitudService solicitudService;
 
-}
+    @GetMapping
+    public ResponseEntity<List<Solicitud>> ListarSolicitudes() {
+        return ResponseEntity.ok(solicitudService.ListarSolicitudes());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Solicitud> obtenerSolicitud(@PathVariable int id) {
+        return solicitudService.obternerPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+    @PostMapping
+    public ResponseEntity<String> registrar(@Valid @RequestBody Solicitud solicitud) {
+        solicitudService.registrarSolicitud(solicitud);
+        return ResponseEntity.ok("Solicitud registrada exitosamente");
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<String> actualizar(@PathVariable int id, @Valid @RequestBody Solicitud solicitud) {
+        solicitud.setId(id);
+        solicitudService.actualizarSolicitud(solicitud);
+        return ResponseEntity.ok("Solicitud actualizada exitosamente");
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> eliminar(@PathVariable int id) {
+        solicitudService.eliminarSolicitud(id);
+        return ResponseEntity.ok("Solicitud eliminada exitosamente");
+    }
+    @GetMapping("/prioridad/{nivel}")
+    public ResponseEntity<List<Solicitud>> filtrarSolicitudesPorPrioridad(@PathVariable String nivel) {
+        return ResponseEntity.ok(solicitudService.filtrarPorPrioridad(nivel));
+    }
+  }

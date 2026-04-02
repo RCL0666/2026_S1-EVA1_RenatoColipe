74969ffcb5df9574f5608bcd314fe2bd4d1fc802
Renato Colipe Lienan <re.colipe@duocuc.cl>
2026-04-02 12:00:58 -0300

    se arreglo el nombre de las variables de las clase Solicitud a minuscula

--- renatocolipe/src/main/java/model/Solicitud.java
+++ renatocolipe/src/main/java/model/Solicitud.java
@@ -9,25 +9,25 @@ public class Solicitud {
     private int id;
 
     @NotBlank(message = "El nombre del paciente es obligatorio")
-    private String Paciente;
+    private String paciente;
 
     @NotBlank(message = "El especialidad es obligatoria")
-    private String Especialidad;
+    private String especialidad;
 
     @NotBlank(message = "El estado es obligatorio")
-    private String Estado; 
+    private String estado; 
 
     @NotNull(message = "El fecha de registro es obligatoria")
-    private LocalDate FechaRegistro;
+    private LocalDate fechaRegistro;
 
     @NotBlank(message = "El Nivel de prioridad es obligatorio")
-    private String NivelPrioridad; 
+    private String nivelPrioridad; 
 
       public Solicitud(int id , String Paciente, String Especialidad, String Estado, LocalDate FechaRegistro, String NivelPrioridad) {
         this.id = id;
-        this.Paciente = Paciente;
-        this.Especialidad = Especialidad;
-        this.Estado = Estado;
+        this.paciente = Paciente;
+        this.especialidad = Especialidad;
+        this.estado = Estado;
         this.FechaRegistro = FechaRegistro;
         this.NivelPrioridad = NivelPrioridad;
     } 
@@ -35,19 +35,19 @@ public class Solicitud {
     public int getId() {return id;}
     public void setId(int id) {this.id = id;}
 
-    public String getPaciente() {return Paciente;}
-    public void setPaciente(String paciente) {this.Paciente = paciente;}
+    public String getPaciente() {return paciente;}
+    public void setPaciente(String paciente) {this.paciente = paciente;}
 
-    public String getEspecialidad() {return Especialidad;}
-    public void setEspecialidad(String especialidad) {this.Especialidad = especialidad;}
+    public String getEspecialidad() {return especialidad;}
+    public void setEspecialidad(String especialidad) {this.especialidad = especialidad;}
 
-    public String getEstado() {return Estado;}
-    public void setEstado(String estado) {this.Estado = estado;}
+    public String getEstado() {return estado;}
+    public void setEstado(String estado) {this.estado = estado;}
 
-    public LocalDate getFechaRegistro() {return FechaRegistro;}
-    public void setFechaRegistro(LocalDate fechaRegistro) {this.FechaRegistro = fechaRegistro;}
+    public LocalDate getFechaRegistro() {return fechaRegistro;}
+    public void setFechaRegistro(LocalDate fechaRegistro) {this.fechaRegistro = fechaRegistro;}
 
     public String getNivelPrioridad() {return NivelPrioridad;}
-    public void setNivelPrioridad(String nivelPrioridad) {this.NivelPrioridad = nivelPrioridad;}
+    public void setNivelPrioridad(String nivelPrioridad) {this.nivelPrioridad = nivelPrioridad;}
 
 }

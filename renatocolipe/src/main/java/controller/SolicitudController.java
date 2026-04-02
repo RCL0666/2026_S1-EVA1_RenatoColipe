cb5f53b4267624a129e682e10c16b6b8c8d28867
Renato Colipe Lienan <re.colipe@duocuc.cl>
2026-04-02 12:25:40 -0300

    Agregue importaciones a la clase SolicitudController

--- renatocolipe/src/main/java/controller/SolicitudController.java
+++ renatocolipe/src/main/java/controller/SolicitudController.java
@@ -1,5 +1,10 @@
 package cl.duoc.renatocolipe.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;                     
+import org.springframework.web.bind.annotation.*;
+
 public class SolicitudController {
 
 }
